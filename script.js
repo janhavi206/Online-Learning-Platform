af637bbc5c1d94bc0c2bce4dd1ee07823616ef38
janhavi206 <142309026+janhavi206@users.noreply.github.com>
2024-07-31 18:13:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
     console.log("Document loaded");
 });
+
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+        alert("Valid email ID");
+    } else {
+        alert("Invalid email ID");
+    }
+}
